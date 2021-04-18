9564c2f65ca85be52c19179b2bd288b07d12a704
Vyshakh <vyshakh.shanthi@paymentsense.com>
2021-04-18 18:35:20 +0100

    - edit copyright.kt

--- .spotless/copyright.kt
+++ .spotless/copyright.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright $YEAR vmadalin.com
+ * Copyright 2021 vyshas
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
